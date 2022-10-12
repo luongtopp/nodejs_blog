05ceffbea91d0494523bc003366b97bd78951723
luongtopp <tranducluong1642002@gmail.com>
2022-10-12 17:24:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/anh', (req, res) => {
+    
+    return res.send('Hello World!')
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
